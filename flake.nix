596dc49e6dad9feba04331fe6bcb382cc5adf7f6
Jacob Chvatal <jake@isnt.online>
2022-01-02 03:12:55 -0500

    fix up to only work on x86 linux

--- flake.nix
+++ flake.nix
@@ -9,74 +9,75 @@
   };
 
   outputs = { self, nixpkgs, rust-overlay, utils, naersk, ... }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        inherit (lib) attrValues;
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ rust-overlay.overlay ];
-        };
-        lib = pkgs.lib;
+    # utils.lib.eachDefaultSystem (system:
+    let
+      system = "x86_64-linux";
 
-        rust_channel = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      inherit (lib) attrValues;
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ rust-overlay.overlay ];
+      };
+      lib = pkgs.lib;
 
-        naersk-lib = naersk.lib."${system}".override {
-          cargo = rust_channel;
-          rustc = rust_channel;
-        };
+      rust_channel = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
-        # The rust server package
-        isntweb-home-server = naersk-lib.buildPackage {
-          pname = "isntweb-home-server";
-          root = ./.;
-        };
+      naersk-lib = naersk.lib."${system}".override {
+        cargo = rust_channel;
+        rustc = rust_channel;
+      };
 
-        # The static front-end as a derivation
-        static-sources = pkgs.stdenv.mkDerivation {
-          name = "isntweb-sources";
-          src = ./.;
+      # The rust server package
+      isntweb-home-server = naersk-lib.buildPackage {
+        pname = "isntweb-home-server";
+        root = ./.;
+      };
 
-          phases = "installPhase";
+      # The static front-end as a derivation
+      static-sources = pkgs.stdenv.mkDerivation {
+        name = "isntweb-sources";
+        src = ./.;
 
-          installPhase = ''
+        phases = "installPhase";
+
+        installPhase = ''
             mkdir -p $out/static
             cp -r ${./static}/* $out/static
           '';
-        };
+      };
 
-        # a script executable passing the root dir to the package
-        isntweb-bundle = pkgs.writeScriptBin
-          # port is the second arg
-          "isntweb-serve" "${isntweb-home-server}/bin/isntweb-home-server ${static-sources}/static 6200";
+      # a script executable passing the root dir to the package
+      isntweb-bundle = pkgs.writeScriptBin
+        # port is the second arg
+        "isntweb-serve" "${isntweb-home-server}/bin/isntweb-home-server ${static-sources}/static 6200";
 
-        isntwebHomeModule = import ./.;
+      isntwebHomeModule = import ./.;
 
+    in rec {
+      # packages.isntweb-home = isntweb-bundle;
+      # apps.isntweb-home = packages.isntweb-home;
+      defaultPackage.${system} = isntweb-bundle;
 
-      in rec {
-        packages.isntweb-home = isntweb-bundle;
-        apps.isntweb-home = packages.isntweb-home;
-        defaultPackage = packages.isntweb-home;
+      nixosModules.isntweb-home = isntwebHomeModule;
+      nixosModule = nixosModules.isntweb-home;
 
-        nixosModules.isntweb-home = isntwebHomeModule;
-        nixosModule = nixosModules.isntweb-home;
+      devShell.${system} = with pkgs; mkShell {
+        buildInputs = [
+          # all of rust unstable
+          rust_channel
+          # fast linking
+          lld
+          # not sure
+          pkg-config
+          # for tbe web
+          wasm-pack
+          # web development
+          nodejs
+          nodePackages.yarn
+        ];
 
-        devShell = with pkgs; mkShell {
-          buildInputs = [
-            # all of rust unstable
-            rust_channel
-            # fast linking
-            lld
-            # not sure
-            pkg-config
-            # for tbe web
-            wasm-pack
-            # web development
-            nodejs
-            nodePackages.yarn
-          ];
-
-          # don't warn for dead code, unused imports or unused variables
-          RUSTFLAGS = "-A dead_code -A unused_imports -A unused_variables";
-        };
-      });
+        # don't warn for dead code, unused imports or unused variables
+        RUSTFLAGS = "-A dead_code -A unused_imports -A unused_variables";
+      };
+    };
 }
