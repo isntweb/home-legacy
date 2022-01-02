bdcc6d557750e6e08e605e0da1d039bbb715e9e1
Jacob Chvatal <jake@isnt.online>
2022-01-02 03:18:19 -0500

    inline module to avoid complexity

--- flake.nix
+++ flake.nix
@@ -51,7 +51,47 @@
         # port is the second arg
         "isntweb-serve" "${isntweb-home-server}/bin/isntweb-home-server ${static-sources}/static 6200";
 
-      isntwebHomeModule = import ./.;
+      # the nixos module taht runs the whole deal!
+      isntwebHomeModule = { config, options, lib, pkgs, ... }:
+        with lib;
+        let cfg = config.isntweb-home;
+            user = "isntweb-home";
+            group = "isntweb-home";
+
+        in {
+          options.isntweb-home = with lib; {
+            enable = mkEnableOption false;
+          };
+
+          config = mkIf cfg.enable {
+            # create a user in which to run the web app
+            users.users.isntweb-home = {
+              inherit group;
+              isSystemUser = true;
+            };
+
+            users.groups.vaultwarden = {};
+
+            # configure a systemd service to launh it
+            systemd.services.isntweb-home = {
+              aliases = [ "isntweb-home" ];
+              after = [ "network.target" ];
+              path = with pkgs; [ openssl ];
+              serviceConfig = {
+                User = user;
+                Group = group;
+                ExecStart = "${isntweb-bundle}/bin/isntweb-serve";
+                PrivateTmp = "true";
+                PrivateDevices = "true";
+                ProtectHome = "true";
+                ProtectSystem = "strict";
+                StateDirectory = "isntweb-home";
+              };
+              wantedBy = [ "multi-user.target" ];
+            };
+          };
+        }
+      ;
 
     in rec {
       # packages.isntweb-home = isntweb-bundle;
